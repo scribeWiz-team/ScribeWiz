5ca92dfed28b4b8aee01a11eb5b80c7ffff8c761
Noé Terrier <terriernoe@gmail.com>
2023-02-25 12:16:34 +0100

    first commit

---
+++ app/src/androidTest/java/com/github/scribeWizTeam/scribewiz/MainActivityTest.kt
@@ -0,0 +1,40 @@
+package com.github.scribeWizTeam.scribewiz
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @get:Rule
+    val rule = activityScenarioRule<MainActivity>()
+
+    @Test
+    fun testExample() {
+        Intents.init()
+
+        val mainNameInteraction = onView(ViewMatchers.withId(R.id.mainName))
+        mainNameInteraction.perform(ViewActions.replaceText("John Doe"))
+
+        val mainGoButtonInteraction = onView(ViewMatchers.withId(R.id.mainGoButton))
+        mainGoButtonInteraction.perform(ViewActions.click())
+
+        Intents.intended(IntentMatchers.hasComponent(GreetingActivity::class.java.name))
+
+        Intents.release()
+    }
+}
---
+++ app/src/main/java/com/github/scribeWizTeam/scribewiz/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.github.scribeWizTeam.scribewiz
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val gotButton = findViewById<Button>(R.id.mainGoButton)
+        val nameText = findViewById<EditText>(R.id.mainName)
+
+        gotButton.setOnClickListener {
+            //set the event you want to perform when button is clicked
+            //you can go to another activity in your app by creating Intent
+            val intent = Intent(this, GreetingActivity::class.java)
+            intent.putExtra("name", nameText.text.toString())
+            startActivity(intent)
+        }
+
+    }
+}
