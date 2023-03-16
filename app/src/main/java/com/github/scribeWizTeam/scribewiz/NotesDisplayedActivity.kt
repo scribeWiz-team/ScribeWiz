a35c45bbfb1b9a5397321553f9b1bf11c62572b7
Louis Barinka <louis.barinka@epfl.ch>
2023-03-16 15:47:15 +0100

    Add function to convert a partition to memory Object
    The memory object is of type Score here since it is what alphaTab
    uses

---
+++ app/src/main/java/com/github/scribeWizTeam/scribewiz/NotesDisplayedActivity.kt
@@ -0,0 +1,51 @@
+package com.github.scribeWizTeam.scribewiz
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import alphaTab.AlphaTabView
+import alphaTab.Settings
+import alphaTab.core.ecmaScript.Uint8Array
+import alphaTab.importer.ScoreLoader
+import alphaTab.model.Score
+import alphaTab.synth.PlayerState
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import java.io.ByteArrayOutputStream
+import kotlin.contracts.ExperimentalContracts
+
+
+class NotesDisplayedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    @ExperimentalContracts //Required by the implementation of the library to work
+    private fun loadInMemoryMusicXML(uri: Uri, settings: Settings?) : Score? {
+        var inMemoryObject : Score? = null
+        try {
+            val fileData = readFileData(uri)
+            inMemoryObject = ScoreLoader.loadScoreFromBytes(fileData, settings)
+            Log.i("AlphaTab", "File loaded: ${inMemoryObject.title}")
+        } catch (e: Exception) {
+            Log.e("AlphaTab", "Failed to load file: $e, ${e.stackTraceToString()}")
+            Toast.makeText(this, "Open File Failed", Toast.LENGTH_LONG).show() //simple feedback in a small popup
+        }
+
+        return inMemoryObject
+    }
+
+    @ExperimentalContracts
+    //Copied-paste from the usage example of alphaTab
+    private fun readFileData(uri: Uri): Uint8Array {
+        val inputStream = contentResolver.openInputStream(uri)
+        inputStream.use {
+            ByteArrayOutputStream().use {
+                inputStream!!.copyTo(it)
+                return Uint8Array(it.toByteArray().asUByteArray())
+            }
+        }
+    }
+
+}
