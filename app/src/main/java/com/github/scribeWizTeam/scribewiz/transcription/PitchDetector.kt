8d34a3c881aad706ac41a599d0020aabfa5e3d32
Baptiste Lambert <blambert@ntymail.com>
2023-03-22 13:40:23 +0100

    improve the signal processing algorithm

--- app/src/main/java/com/github/scribeWizTeam/scribewiz/transcription/PitchDetector.kt
+++ app/src/main/java/com/github/scribeWizTeam/scribewiz/transcription/PitchDetector.kt
@@ -27,6 +27,21 @@ class PitchDetector(val samplingFreq: Frequency){
         return energy
     }
 
+    private fun square_sum(signal: Signal, lag: Int): Energy{
+        var energy = 0.0
+        for (i in 0 until signal.size - lag){
+            energy += signal[i].pow(2)+signal[i+lag].pow(2)
+        }
+        return energy
+    }
+
+    private fun normal_square_diff(signal: Signal, lag: Int): Energy{
+        val r = autocorrelation(signal, lag)
+        val m = square_sum(signal, lag)
+        val n = 2*r/m
+        return max(-1.0, min(1.0, n))
+    }
+
     private fun lag_to_freq(lag: Int): Frequency {
         return samplingFreq / lag
     }
@@ -38,19 +53,35 @@ class PitchDetector(val samplingFreq: Frequency){
     fun detect_pitch(signal: Signal): Frequency? {
         val highLag = freq_to_lag(50.0)
         val lowLag = freq_to_lag(4000.0)
-        val energy = autocorrelation(signal, 0)
-        var bestEnergy = 0.0
+        var bestCorr = -1.0
         var bestLag : Int? = null
-        for (lag in highLag downTo lowLag){
-            val energy_shifted = autocorrelation(signal, lag)
-            if (energy_shifted > bestEnergy){
-                bestEnergy = energy_shifted
-                bestLag = lag
+        var candidateMax : List<Pair<Int, Energy>> = emptyList()
+        for (lag in lowLag .. highLag){
+            val currentCorr = normal_square_diff(signal, lag)
+            if (currentCorr > 0){
+                if (currentCorr > bestCorr){
+                    bestCorr = currentCorr
+                    bestLag = lag
+                }
+            } else {
+                if (bestLag != null){
+                    candidateMax += Pair(bestLag, bestCorr)
+                    bestLag = null
+                    bestCorr = -1.0
+                }
             }
         }
-        if (bestLag == null || bestEnergy < 0.9*energy){
+        if (candidateMax.size == 0){
             return null
         }
-        return lag_to_freq(bestLag)
+        val (lagOfMax, maxCorr) = candidateMax.maxBy { it.second }
+        val corrThreshold = 1.0
+        val fundamental = candidateMax.find { it.second >= corrThreshold }
+        if (fundamental == null){
+            return null
+        }
+        val (fundamentalLag, fundamentalCorr) = fundamental
+        val fundFreq = lag_to_freq(fundamentalLag)
+        return fundFreq
     }
 }
--- app/src/test/java/com/github/scribeWizTeam/scribewiz/transcription/PitchDetectorTest.kt
+++ app/src/test/java/com/github/scribeWizTeam/scribewiz/transcription/PitchDetectorTest.kt
@@ -32,23 +32,24 @@ class PitchDetectorTest {
 
     @Test
     fun silent_signal_gives_no_pitch() {
-        val signal = List(4000, { 0.0 })
+        val signal = List(2000, { 0.0 })
         assertNull(detector.detect_pitch(signal))
     }
 
     @Test
     fun correctly_detects_the_pitch_of_a_pure_sine_wave() {
         val signal_freq = 440.0
-        val signal = List(4400, { sin(2*PI*signal_freq*it/sampleFreq) })
+        val signal = List(2000, { sin(2*PI*signal_freq*it/sampleFreq) })
         assertEquals(signal_freq, detector.detect_pitch(signal))
     }
 
     @Test
     fun correctly_detects_the_fundamental_frequency_of_a_composed_signal() {
         val signal_freq = 440.0
-        val signal = List(4400, { sin(2*PI*signal_freq*it/sampleFreq)
-                                 +0.00001*sin(2*PI*2*signal_freq*it/sampleFreq)
-                                 // +0.00001*sin(2*PI*8*signal_freq*it/sampleFreq)
+        val signal = List(2000, { 
+                                 sin(2*PI*signal_freq*it/sampleFreq)
+                                 +0.6*sin(2*PI*2*signal_freq*it/sampleFreq+1.3)
+                                 +0.3*sin(2*PI*3*signal_freq*it/sampleFreq+2)
                                 })
         assertEquals(signal_freq, detector.detect_pitch(signal))
     }
