1fdba959e43d2b6c4a1bf751c4642d69228be86c
Christopher Williams <christopher.williams@epfl.ch>
2023-03-23 20:38:55 +0100

    fixing button xml in main activity

--- app/src/main/java/com/github/scribeWizTeam/scribewiz/MainActivity.kt
+++ app/src/main/java/com/github/scribeWizTeam/scribewiz/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.github.scribeWizTeam.scribewiz
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
-import android.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
