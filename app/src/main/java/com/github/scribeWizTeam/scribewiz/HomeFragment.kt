ad9ef8e29a892933d2882676d5fbe598ea1aad3d
Stefan Popescu <stefan-gabriel.popescu@epfl.ch>
2023-03-26 17:08:55 +0200

    Unifying compose usage within fragments Record and share not done yet

--- app/src/main/java/com/github/scribeWizTeam/scribewiz/HomeFragment.kt
+++ app/src/main/java/com/github/scribeWizTeam/scribewiz/HomeFragment.kt
@@ -4,6 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 
 class HomeFragment(contentLayoutId: Int) : Fragment(contentLayoutId) {
@@ -12,8 +23,26 @@ class HomeFragment(contentLayoutId: Int) : Fragment(contentLayoutId) {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        return ComposeView(requireContext()).apply {
+            // Dispose the Composition when viewLifecycleOwner is destroyed
+            setViewCompositionStrategy(
+                ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner)
+            )
+
+            setContent {
+                Column(
+                    modifier = Modifier.fillMaxSize().padding(all = 8.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Home",
+                        style = MaterialTheme.typography.h4,
+                        fontSize = 50.sp
+                    )
+                }
+            }
+        }
     }
 
 }
--- app/src/main/java/com/github/scribeWizTeam/scribewiz/NavigationActivity.kt
+++ app/src/main/java/com/github/scribeWizTeam/scribewiz/NavigationActivity.kt
@@ -66,8 +66,8 @@ class NavigationActivity : AppCompatActivity(), NavigationView.OnNavigationItemS
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Code to handle the navigation view item click event
         when (item.itemId) {
-            R.id.nav_help -> showFragment(HelpFragment(0))
             R.id.nav_home -> showFragment(HomeFragment(0))
+            R.id.nav_help -> showFragment(HelpFragment(0))
             R.id.nav_library -> showFragment(LibraryFragment(0))
             R.id.nav_rec -> showFragment(RecFragment(0))
             R.id.nav_settings -> showFragment(SettingsFragment(0))
