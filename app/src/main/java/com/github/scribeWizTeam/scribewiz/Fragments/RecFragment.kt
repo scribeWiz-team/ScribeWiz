00ae7a06598da781dcd1ab2dc59b3f0601da3510
Noé Terrier <terriernoe@gmail.com>
2023-04-27 16:33:22 +0200

    feat: metronome added

--- app/src/main/java/com/github/scribeWizTeam/scribewiz/Fragments/RecFragment.kt
+++ app/src/main/java/com/github/scribeWizTeam/scribewiz/Fragments/RecFragment.kt
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.media.AudioFormat
 import android.media.AudioRecord
+import android.media.MediaPlayer
 import android.media.MediaRecorder
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -13,49 +14,51 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.github.scribeWizTeam.scribewiz.R
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
 
 class RecFragment(contentLayoutId: Int) : Fragment(contentLayoutId) {
 
+    companion object {
+        private const val REQUEST_RECORD_AUDIO_PERMISSION = 200 //request code for permission
+        private const val MILLIS_IN_FUTURE = 9999999L //number of milliseconds maximum record time
+        private const val COUNT_DOWN_INTERVAL = 1000L //number of milliseconds between each tick
+        private const val SAMPLE_RATE_IN_HZ = 44100
+    }
 
     private val audioSource = MediaRecorder.AudioSource.MIC
-    private val sampleRateInHz = 44100
     private val channelConfig = AudioFormat.CHANNEL_IN_MONO
     private val audioFormat = AudioFormat.ENCODING_PCM_16BIT
-    private val bufferSize = AudioRecord.getMinBufferSize(sampleRateInHz, channelConfig, audioFormat)
+    private val bufferSize = AudioRecord.getMinBufferSize(Companion.SAMPLE_RATE_IN_HZ, channelConfig, audioFormat)
 
     private lateinit var audioRecorder: AudioRecord //media recorder to record audio
+    private lateinit var mediaPlayer: MediaPlayer//media player to play sound
 
     private lateinit var timer: CountDownTimer //timer to show recording time
 
     private lateinit var outputFile: File
     private lateinit var outputFilePath: String
-    var isRecording = false //boolean to check if recording is in progress
-    val REQUEST_RECORD_AUDIO_PERMISSION = 200 //request code for permission
-    val MILLISINFUTURE = 9999999L //number of milliseconds maximum record time
-    val COUNT_DOWN_INTERVAL = 1000L //number of milliseconds between each tick
+    private var isRecording = false //boolean to check if recording is in progress
+
+    private var metronomeIsPlaying = false
 
     constructor() : this(0) {
         // Default constructor
@@ -69,57 +72,119 @@ class RecFragment(contentLayoutId: Int) : Fragment(contentLayoutId) {
         //check if the app has permission to record audio
         checkPermission()
 
+        mediaPlayer = MediaPlayer.create(context, R.raw.tick)
+
         return ComposeView(requireContext()).apply {
             setContent {
 
-                var counterText by remember { mutableStateOf("00:00") }
-                var recordButtonText by remember { mutableStateOf("Start recording") }
-
+                val counterText = remember { mutableStateOf("00:00") }
+                val recordButtonText = remember { mutableStateOf("Start recording") }
+                val metronomeButtonText = remember { mutableStateOf("Start metronome") }
+                val tempoValue = remember { mutableStateOf("60") }
 
                 Box(
-                    modifier = Modifier.fillMaxSize().padding(all = 8.dp),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(all = 8.dp),
                     contentAlignment = Alignment.Center
                 ) {
                     Column (horizontalAlignment = CenterHorizontally) {
-                        Text(text = counterText, fontSize = 24.sp, modifier = Modifier.padding(10.dp), textAlign = TextAlign.Center)
-                        Button(modifier = Modifier
-                            .height(40.dp)
-                            .width(180.dp),
-                            onClick = {
-                                if (!isRecording) {
-                                    // Set the timer
-                                    timer = object : CountDownTimer(MILLISINFUTURE, COUNT_DOWN_INTERVAL) {
-                                        override fun onTick(millisUntilFinished: Long) {
-                                            counterText = ((MILLISINFUTURE - millisUntilFinished) / COUNT_DOWN_INTERVAL).toString()
-                                        }
-
-                                        override fun onFinish() {
-                                            // Do nothing
-                                        }
-                                    }
-
-                                    //start recording
-                                    recordButtonText = "Stop recording"
-                                    startRecording()
-                                } else {
-                                    //stop recording
-                                    // Set the recording time to 0
-                                    counterText = "Recording saved!"
-                                    recordButtonText = "Start recording"
-                                    stopRecording()
-                                }
-                        }) {
-                            Icon(
-                                Icons.Filled.PlayArrow,
-                                contentDescription = "Favorite",
-                                modifier = Modifier.size(ButtonDefaults.IconSize)
-                            )
-                            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
-                            Text(recordButtonText)
+                        Text(text = counterText.value, fontSize = 24.sp, modifier = Modifier.padding(10.dp), textAlign = TextAlign.Center)
+                        PlayButton(recordButtonText) {
+                            switchRecordState(counterText, recordButtonText)
                         }
+                        PlayButton(metronomeButtonText) {
+                            switchMetronomeState(metronomeButtonText, tempoValue)
+                        }
+                        OutlinedTextField(tempoValue.value,
+                            {tempoValue.value = it},
+                            modifier = Modifier
+                                .height(70.dp)
+                                .width(190.dp)
+                                .padding(5.dp),
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                            label = { Text(text = "Tempo")})
                     }
                 }
             }
+        }
+    }
+
+    @Composable
+    private fun PlayButton(text: MutableState<String>,  onClick : () -> Unit) {
+        Button(
+            modifier = Modifier
+                .height(50.dp)
+                .width(190.dp)
+                .padding(5.dp),
+            onClick = onClick) {
+            Icon(
+                Icons.Filled.PlayArrow,
+                contentDescription = "play",
+                modifier = Modifier.size(ButtonDefaults.IconSize)
+            )
+            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+            Text(text.value)
+        }
+    }
+
+    private fun onTickTimer(interval: Long, onTick: (Long) -> Unit) : CountDownTimer {
+        return object : CountDownTimer(MILLIS_IN_FUTURE, interval) {
+            override fun onTick(millisUntilFinished: Long) {
+                onTick(millisUntilFinished)
+            }
+
+            override fun onFinish() {
+                // Do nothing
+            }
+        }
+    }
+
+    private fun switchMetronomeState(metronomeButtonText: MutableState<String>, tempoValue: MutableState<String>) {
+        if (metronomeIsPlaying) {
+            metronomeButtonText.value = "Start metronome"
+            metronomeIsPlaying = false
+            timer.cancel()
+        } else {
+            val tempo : Long = try {
+               tempoValue.value.toLong()
+            } catch (_: java.lang.NumberFormatException) {
+                60L
+            }
+
+            timer = onTickTimer(1000 * 60 / tempo) {
+                if (mediaPlayer.isPlaying) {
+                    mediaPlayer.pause()
+                    mediaPlayer.seekTo(0)
+                }
+                mediaPlayer.start()
+            }
+            timer.start()
+            metronomeIsPlaying = true
+            metronomeButtonText.value = "Stop metronome"
+        }
+    }
+
+    private fun switchRecordState(counterText : MutableState<String>, recordButtonText : MutableState<String>) {
+        if (!isRecording) {
+            // Set the timer
+            timer = onTickTimer(COUNT_DOWN_INTERVAL) { millisUntilFinished ->
+                counterText.value = ((MILLIS_IN_FUTURE - millisUntilFinished) / Companion.COUNT_DOWN_INTERVAL).toString()
+            }
+
+            //start recording
+            recordButtonText.value = "Stop recording"
+            // Start the timer
+            timer.start()
+            startRecording()
+        } else {
+            //stop recording
+            // Stop the timer
+            timer.cancel()
+            // Set the recording time to 0
+            counterText.value = "Recording saved!"
+            recordButtonText.value = "Start recording"
+            stopRecording()
         }
     }
 
@@ -133,7 +198,7 @@ class RecFragment(contentLayoutId: Int) : Fragment(contentLayoutId) {
             ActivityCompat.requestPermissions(
                 requireActivity(),
                 arrayOf(Manifest.permission.RECORD_AUDIO),
-                REQUEST_RECORD_AUDIO_PERMISSION
+                Companion.REQUEST_RECORD_AUDIO_PERMISSION
             )
         }
     }
@@ -146,7 +211,7 @@ class RecFragment(contentLayoutId: Int) : Fragment(contentLayoutId) {
         //set the result of the permission request
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         //check if the request code is same as the one we sent
-        if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
+        if (requestCode == Companion.REQUEST_RECORD_AUDIO_PERMISSION) {
             //check if the permission is granted
             if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
                 //if not, show a toast "Permission Denied"
@@ -166,7 +231,8 @@ class RecFragment(contentLayoutId: Int) : Fragment(contentLayoutId) {
     @SuppressLint("Permissions are checked before calling this method", "MissingPermission")
     private fun startRecording() {
         // Initialize the AudioRecord
-        audioRecorder = AudioRecord(audioSource, sampleRateInHz, channelConfig, audioFormat, bufferSize)
+        audioRecorder = AudioRecord(audioSource,
+            Companion.SAMPLE_RATE_IN_HZ, channelConfig, audioFormat, bufferSize)
 
         // Set the output file path
         outputFilePath = getOutputFilePath()
@@ -193,9 +259,6 @@ class RecFragment(contentLayoutId: Int) : Fragment(contentLayoutId) {
             }
             outputStream.close()
         }).start()
-
-        // Start the timer
-        timer.start()
     }
 
     // Method to stop recording
@@ -206,7 +269,14 @@ class RecFragment(contentLayoutId: Int) : Fragment(contentLayoutId) {
         audioRecorder.release()
         // Set recording not in progress
         isRecording = false
-        // Stop the timer
-        timer.cancel()
+    }
+
+
+    override fun onDestroy() {
+        if (this::mediaPlayer.isInitialized) {
+            mediaPlayer.stop()
+            mediaPlayer.release()
+        }
+        super.onDestroy()
     }
 }
